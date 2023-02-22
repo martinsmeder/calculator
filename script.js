0f409b9d4d4551b201eb9af3fb26baa4489f3145
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-02-22 17:51:39 +0100

    Added keyboard event handling and organized code

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ let sum = null;
 let lastClick = null; 
 
 // TO DO:
-// 1 Keyboard eventListeners 
-// 2 Style 
+// 1. Style 
 // https://www.section.io/engineering-education/building-a-calculator-a-javascript-project-for-beginners/
 
+// handle click events
 numbers.forEach(button => {
     button.addEventListener('click', clearResult);
     button.addEventListener('click', getNumberClick);
@@ -34,6 +34,62 @@ function backspace() {
     display.value = display.value.slice(0, -1);
 };
 
+// handle keyboard events
+document.addEventListener("keydown", (e) => {
+    if (e.key >= 0 && e.key <= 9) {
+      let button = document.querySelector(`div #numbers > button[value="${e.key}"]`);
+      if (button) {
+        clearResult();
+        getNumberClick({target: button});
+      }
+    }
+    else if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/") {
+      let button = document.querySelector(`div #symbols > button[value="${e.key}"]`);
+      if (button) {
+        calculate({target: button});
+      }
+    }
+    else if (e.key === ".") {
+      let button = document.querySelector(`div #numbers > button[value="."]`);
+      if (button) {
+        clearResult();
+        getNumberClick({target: button});
+      }
+    }
+    else if (e.key === "=" || e.key === "Enter") {
+      let button = document.querySelector(`div #symbols > button[value="="]`);
+      if (button) {
+        calculate({target: button});
+      }
+    }
+    else if (e.key === "Backspace") {
+      backspace();
+    }
+    else if (e.key === "Escape") {
+      clearEverything();
+    }
+    console.log(e)
+});
+
+// handle click events
+numbers.forEach(button => {
+    button.addEventListener('click', clearResult);
+    button.addEventListener('click', getNumberClick);
+});
+
+symbols.forEach(button => {
+    button.addEventListener('click', calculate);
+});
+
+plusMinus.addEventListener('click', convertNumber)
+backspaceBtn.addEventListener('click', backspace);
+clearBtn.addEventListener('click', clearEverything);
+
+function backspace() {
+    display.value = display.value.slice(0, -1);
+};
+
+// functions
 function calculate(e) {
     if (lastClick === '+' || lastClick === '-' || lastClick === '*' || lastClick === '/') {
         updateCalcDisplay;
@@ -49,99 +105,22 @@ function calculate(e) {
     } 
 };
 
-function clearResult() {
-    if (!(typeof lastClick === 'number')) {
-        clearDisplay()
-    }
-};
-
-function getNumberClick(e) {
-    lastClick = parseInt(e.target.value);
-    updateDisplay(e)
-};
-
-function updateNumber() {
-    if (a === null) {
-        a = parseFloat(display.value);
-    } else if (!(a === null) && b === null) {
-        b = parseFloat(display.value);
-    } else if (!(a === null) && !(b === null)) {
-        a = sum;
-        b = parseFloat(display.value);
-    }
-    clearDisplay()
-};
-
-function clearEverything() {
-    clearDisplay()
-    clearCalcDisplay()
-    a = null;
-    b = null;
-    sum = null;
-    operator = null;
-};
-
-function clearDisplay() {
-    display.value = null;
-};
-
-function clearCalcDisplay() {
-    calcDisplay.value = null;
-}
-
-function backspace() {
-    display.value = display.value.slice(0, -1);
-};
-
-function updateDisplay(e) {
-    display.value += e.target.value;  
-
-};
-
-function convertNumber(num) {
-    num = parseFloat(display.value);
-    if (num > 0) {
-        display.value = -num;
-    } else if (num < 0) {
-        display.value = -(num);
-    } else if (num = 0) {
-        display.value = num
-    } 
-}
-
-function updateCalcDisplay() {
-    if (a === null) {
-        a = parseFloat(a)
+function operate(a, operator, b) {
+    if (a === null || Number.isNaN(a)) {
+        clearEverything()
+        display.value = "Start with a number! Click: AC"
+        return;
     }
 
-    if (!Number.isNaN(a) && b === null) {
-        calcDisplay.value = `${a} ${operator}`;
-    } else if (!Number.isNaN(a) && b !== null && Number.isInteger(sum)) {
-        calcDisplay.value = `${sum} ${operator}`;
-    } else if (!Number.isNaN(a) && b !== null && !Number.isInteger(sum)) {
-        calcDisplay.value = `${sum.toFixed(2)} ${operator}`;
-    } else if (Number.isNaN(a)) {
-        calcDisplay.value = `${operator}`;
-    }
-}; 
-
-function showResult() {
-    if (!Number.isInteger(sum) && !(sum === null)) {
-        display.value = sum.toFixed(2);
-    } else if (Number.isInteger(sum)) {
-        display.value = sum;
-    }
-};
-
-function saveOperator(e) {
-    if (lastClick === '+' || lastClick === '-' || lastClick === '*' || lastClick === '/') {
-        updateCalcDisplay()
-        lastClick = e.target.value;
-        operator = e.target.value;
-    } else {
-        lastClick = e.target.value;
-        operator = e.target.value;
-    }
+    if (operator === '+') {
+        sum = add(a, b);
+    } else if (operator === '-') {
+        sum = subtract(a, b);
+    } else if (operator === '*') {
+        sum = multiply(a, b);
+    } else if (operator === '/') {
+        sum = divide(a, b);
+    }  
 };
 
 function add(a, b) {
@@ -166,20 +145,96 @@ function divide(a, b) {
     }
 };
 
-function operate(a, operator, b) {
-    if (a === null || Number.isNaN(a)) {
-        clearEverything()
-        display.value = "Start with a number! Click: AC"
-        return;
+function saveOperator(e) {
+    if (lastClick === '+' || lastClick === '-' || lastClick === '*' || lastClick === '/') {
+        updateCalcDisplay()
+        lastClick = e.target.value;
+        operator = e.target.value;
+    } else {
+        lastClick = e.target.value;
+        operator = e.target.value;
+    }
+};
+
+function showResult() {
+    if (!Number.isInteger(sum) && !(sum === null)) {
+        display.value = sum.toFixed(2);
+    } else if (Number.isInteger(sum)) {
+        display.value = sum;
+    }
+};
+
+function getNumberClick(e) {
+    lastClick = parseInt(e.target.value);
+    updateDisplay(e)
+};
+
+function updateNumber() {
+    if (a === null) {
+        a = parseFloat(display.value);
+    } else if (!(a === null) && b === null) {
+        b = parseFloat(display.value);
+    } else if (!(a === null) && !(b === null)) {
+        a = sum;
+        b = parseFloat(display.value);
+    }
+    clearDisplay()
+};
+
+function updateDisplay(e) {
+    display.value += e.target.value;  
+
+};
+
+function updateCalcDisplay() {
+    if (a === null) {
+        a = parseFloat(a)
     }
 
-    if (operator === '+') {
-        sum = add(a, b);
-    } else if (operator === '-') {
-        sum = subtract(a, b);
-    } else if (operator === '*') {
-        sum = multiply(a, b);
-    } else if (operator === '/') {
-        sum = divide(a, b);
-    }  
+    if (!Number.isNaN(a) && b === null) {
+        calcDisplay.value = `${a} ${operator}`;
+    } else if (!Number.isNaN(a) && b !== null && Number.isInteger(sum)) {
+        calcDisplay.value = `${sum} ${operator}`;
+    } else if (!Number.isNaN(a) && b !== null && !Number.isInteger(sum)) {
+        calcDisplay.value = `${sum.toFixed(2)} ${operator}`;
+    } else if (Number.isNaN(a)) {
+        calcDisplay.value = `${operator}`;
+    }
+}; 
+
+function backspace() {
+    display.value = display.value.slice(0, -1);
 };
+
+function clearResult() {
+    if (!(typeof lastClick === 'number')) {
+        clearDisplay()
+    }
+};
+
+function clearDisplay() {
+    display.value = null;
+};
+
+function clearCalcDisplay() {
+    calcDisplay.value = null;
+};
+
+function clearEverything() {
+    clearDisplay()
+    clearCalcDisplay()
+    a = null;
+    b = null;
+    sum = null;
+    operator = null;
+};
+
+
+
+
+
+
+
+
+
+
